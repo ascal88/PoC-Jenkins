72410ed408220c15aac6d63067dcacb07f839990
pasquale gigliello <p.gigliello@gmail.com>
2019-12-09 00:02:52 +0100

    First Commit

---
+++ src/main/java/it/reply/demo/RestController.java
@@ -0,0 +1,12 @@
+package it.reply.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping(value = "/**")
+    public String hello() {
+        return "Hello World";
+    }
+}
