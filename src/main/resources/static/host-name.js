cb88c20ad5e6f0d2e322e59d75e17358e06d28c0
pasquale gigliello <p.gigliello@gmail.com>
2019-12-14 19:30:21 +0100

    kube url

--- src/main/resources/static/host-name.js
+++ src/main/resources/static/host-name.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://localhost:8080/hostname"
+        url: "http://192.168.99.100:30002/hostname"
     }).then(function(data) {
        document.getElementById('hostname').append(data)
     });
